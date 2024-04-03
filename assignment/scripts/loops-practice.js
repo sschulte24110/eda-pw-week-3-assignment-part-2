6cc572b16488e9991ca4c85c5951dc968b6fe67a
Scott Schulte <scott.r.schulte@gmail.com>
2024-04-03 14:02:02 -0500

    Remove semicolons after closing curly braces.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -8,21 +8,21 @@ console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
 for (let i = 0; i < 4; i++) {
   console.log(i);
-};
+}
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 for (let i = 0; i < 6; i++) {
   console.log(i);
-};
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
 for (let i = 3; i < 6; i++) {
   console.log(i);
-};
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
@@ -31,13 +31,13 @@ for (let i = 2; i < 11; i++) {
   if (i % 2 === 0) {
     console.log(i);
   }
-};
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 for (let i = 5; i >= 0; i--) {
   console.log(i);
-};
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -47,7 +47,7 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 console.log('Some stars:');
 for (star of stars) {
   console.log(star);
-};
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
@@ -58,7 +58,7 @@ let i = 0;
 while (i < stars.length) {
   console.log(stars[i]);
   i++;
-};
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
@@ -66,7 +66,7 @@ let j = 0;
 while (j <= 5) {
   console.log(j);
   j++;
-};
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
@@ -74,4 +74,4 @@ let k = 10;
 while (k >= 5) {
   console.log(k);
   k--;
-};
+}
